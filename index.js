80449c4df7f4198b3fd757ac8f92baaff68c5643
Suyash Mishra <suyashmishra1008@gmail.com>
2025-10-02 17:06:41 +0530

    token verification

--- index.js
+++ index.js
@@ -2,30 +2,31 @@ const express = require('express')
 const connection = require('./connection/connection')
 const { postCompany, getCompany, deleteCompany, updateCompany } = require('./controllers/companyController')
 const { singIn, singUp } = require('./controllers/authController')
+const { checkToken } = require('./middleware/verifyToken')
 const app = express()
 const port = 3000
 
 connection()
-app.use(express.json());          
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // GET :  company data 
-app.get('/', getCompany)
+app.get('/', checkToken, getCompany)
 
 // POST : create company data  
 app.post("/create", postCompany)
- 
+
 // PATCH : update company data 
-app.patch("/update/:id" , updateCompany)
+app.patch("/update/:id", updateCompany)
 
 // DELETE : delete company data 
-app.delete("/delete/:id" , deleteCompany)
+app.delete("/delete/:id", deleteCompany)
 
 // POST : Create User 
 app.post("/create_user", singUp)
 
 // POST : Login 
-app.post("/login" ,singIn )
+app.post("/login", singIn)
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
---
+++ middleware/verifyToken.js
@@ -0,0 +1,18 @@
+var jwt = require('jsonwebtoken');
+
+const checkToken = (req, res, next) => {
+    const header = req.headers.authorization
+    const token = header.split(" ")
+    try {
+        var decoded = jwt.verify(token[1], 'secret');
+        console.log(decoded)
+        if (decoded) {
+            next()
+        }
+    } catch (err) {
+        return res.status(401).json({ message: "Token Not Found" })
+
+    }
+
+}
+module.exports = { checkToken }
